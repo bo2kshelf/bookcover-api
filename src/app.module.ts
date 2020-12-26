399166826790fbc29a186a2d419dacb859ccaa7d
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-27 05:49:34 +0900

    refactor: Code firstに変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import {Module} from '@nestjs/common';
 import {GraphQLFederationModule} from '@nestjs/graphql';
 import {BooksModule} from './books/books.module';
+import {Book} from './books/entity/book.entity';
 
 @Module({
   imports: [
     GraphQLFederationModule.forRoot({
-      typePaths: ['**/schema.graphqls'],
+      autoSchemaFile: true,
+      buildSchemaOptions: {
+        orphanedTypes: [Book],
+      },
     }),
     BooksModule,
   ],
