0177f3ff3f118dee860708a9e43db12835488133
SnO2WMaN <sno2wman.official@gmail.com>
2020-12-26 19:17:19 +0900

    chore: 環境構築

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import {Module} from '@nestjs/common';
+import {GraphQLFederationModule} from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLFederationModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
+})
+export class AppModule {}
