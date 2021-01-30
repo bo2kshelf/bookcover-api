ba4f789fa5f60935fdb6b39ac442d481712449a0
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-30 23:13:13 +0900

    feat: 楽天APIも考慮した書影の取得

--- src/books/test/small/books.service.test.ts
+++ src/books/test/small/books.service.test.ts
@@ -1,6 +1,10 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {OpenBDService} from '../../../openbd/openbd.service';
+import {RakutenService} from '../../../rakuten/rakuten.service';
 import {BooksService} from '../../books.service';
+
+jest.mock('../../../openbd/openbd.service');
+jest.mock('../../../rakuten/rakuten.service');
 
 describe(BooksService.name, () => {
   let module: TestingModule;
@@ -8,22 +12,16 @@ describe(BooksService.name, () => {
   let booksService: BooksService;
 
   let openbdService: OpenBDService;
+  let rakutenService: RakutenService;
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
-      providers: [
-        BooksService,
-        {
-          provide: OpenBDService,
-          useValue: {
-            getBookCover() {},
-          },
-        },
-      ],
+      providers: [BooksService, OpenBDService, RakutenService],
     }).compile();
 
     booksService = module.get<BooksService>(BooksService);
     openbdService = module.get<OpenBDService>(OpenBDService);
+    rakutenService = module.get<RakutenService>(RakutenService);
   });
 
   afterEach(async () => {
@@ -39,20 +37,30 @@ describe(BooksService.name, () => {
   });
 
   describe('getCover()', () => {
-    it('OpenBDServiceが正常な値を返却した場合それを返す', async () => {
-      jest
-        .spyOn(openbdService, 'getBookCover')
-        .mockResolvedValueOnce('https://cover.openbd.jp/9784781618968.jpg');
+    it.each([
+      [
+        // openBDを優先
+        'https://cover.openbd.jp/9784781618968.jpg',
+        'https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2460/9784832272460.jpg',
+        'https://cover.openbd.jp/9784781618968.jpg',
+      ],
+      [
+        null,
+        'https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2460/9784832272460.jpg',
+        'https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2460/9784832272460.jpg',
+      ],
+      [
+        'https://cover.openbd.jp/9784781618968.jpg',
+        null,
+        'https://cover.openbd.jp/9784781618968.jpg',
+      ],
+      [null, null, null],
+    ])('%#', async (openBD, rakuten, expected) => {
+      jest.spyOn(openbdService, 'getBookCover').mockResolvedValueOnce(openBD);
+      jest.spyOn(rakutenService, 'getBookCover').mockResolvedValueOnce(rakuten);
 
       const actual = await booksService.getCover({isbn: '9784781618968'});
-      expect(actual).toBe('https://cover.openbd.jp/9784781618968.jpg');
-    });
-
-    it('OpenBDServiceがnullを返却した場合nullを返す', async () => {
-      jest.spyOn(openbdService, 'getBookCover').mockResolvedValueOnce(null);
-
-      const actual = await booksService.getCover({isbn: '9784781618968'});
-      expect(actual).toBeNull();
+      expect(actual).toBe(expected);
     });
   });
 });
