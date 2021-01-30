b57f3bc0dc55feae87b3c44996c62795887578f4
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-31 02:53:51 +0900

    feat: In-memoryでキャッシュ

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,20 +1,36 @@
-import {Injectable} from '@nestjs/common';
+import {CACHE_MANAGER, Inject, Injectable} from '@nestjs/common';
+import {Cache} from 'cache-manager';
+import {promisify} from 'util';
 import {OpenBDService} from '../openbd/openbd.service';
 import {RakutenService} from '../rakuten/rakuten.service';
 
 @Injectable()
 export class BooksService {
   constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+
     private readonly oepnBDService: OpenBDService,
     private readonly rakutenService: RakutenService,
   ) {}
 
   async getCover({isbn}: {isbn?: string}): Promise<string | null> {
     if (isbn) {
-      return (
+      const cached = await promisify<string, string>(this.cacheManager.get)(
+        isbn,
+      )
+        .then((value) => value)
+        .catch(() => null);
+
+      if (cached) return cached;
+
+      const fetched =
         (await this.oepnBDService.getBookCover(isbn)) ||
-        this.rakutenService.getBookCover(isbn)
-      );
+        (await this.rakutenService.getBookCover(isbn)) ||
+        null;
+
+      if (fetched)
+        return this.cacheManager.set(isbn, fetched, {ttl: 60 * 60 * 24 * 7});
     }
     return null;
   }
