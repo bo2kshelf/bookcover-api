be5e3c17386aeed355a2f1352c863279e46955bc
SnO₂WMaN <sno2wman.official@gmail.com>
2021-01-31 12:55:25 +0900

    feat: 書影をキャッシュする (#12)

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,11 +1,11 @@
-import {Module} from '@nestjs/common';
+import {CacheModule, Module} from '@nestjs/common';
 import {OpenBDModule} from '../openbd/openbd.module';
 import {RakutenModule} from '../rakuten/rakuten.module';
 import {BooksResolver} from './books.resolver';
 import {BooksService} from './books.service';
 
 @Module({
-  imports: [OpenBDModule, RakutenModule],
+  imports: [CacheModule.register(), OpenBDModule, RakutenModule],
   providers: [BooksService, BooksResolver],
   exports: [BooksService],
 })
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,20 +1,37 @@
-import {Injectable} from '@nestjs/common';
+import {CACHE_MANAGER, Inject, Injectable} from '@nestjs/common';
+import {Cache} from 'cache-manager';
+import {promisify} from 'util';
 import {OpenBDService} from '../openbd/openbd.service';
 import {RakutenService} from '../rakuten/rakuten.service';
 
 @Injectable()
 export class BooksService {
   constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+
     private readonly oepnBDService: OpenBDService,
     private readonly rakutenService: RakutenService,
   ) {}
 
+  async getCachedByISBN(isbn: string): Promise<string | null> {
+    return promisify<string, string | undefined>(this.cacheManager.get)(isbn)
+      .then((value) => value || null)
+      .catch(() => null);
+  }
+
   async getCover({isbn}: {isbn?: string}): Promise<string | null> {
     if (isbn) {
-      return (
+      const cached = await this.getCachedByISBN(isbn);
+      if (cached) return cached;
+
+      const fetched =
         (await this.oepnBDService.getBookCover(isbn)) ||
-        this.rakutenService.getBookCover(isbn)
-      );
+        (await this.rakutenService.getBookCover(isbn)) ||
+        null;
+
+      if (fetched)
+        return this.cacheManager.set(isbn, fetched, {ttl: 60 * 60 * 24 * 7});
     }
     return null;
   }
