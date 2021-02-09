7db3f0a5ac654ca7db783f79bdf8e84bf06e1f0f
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-02-09 12:41:53 +0900

    feat: imageproxyでプロキシする

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,13 +1,21 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {ExcludeModule} from '../exclude/exclude.module';
 import {OpenBDModule} from '../openbd/openbd.module';
 import {RakutenModule} from '../rakuten/rakuten.module';
 import {RedisCacheModule} from '../redis-cache/redis-cache.module';
+import {BooksConfig} from './books.config';
 import {BooksResolver} from './books.resolver';
 import {BooksService} from './books.service';
 
 @Module({
-  imports: [ExcludeModule, RedisCacheModule, OpenBDModule, RakutenModule],
+  imports: [
+    ConfigModule.forFeature(BooksConfig),
+    RedisCacheModule,
+    ExcludeModule,
+    OpenBDModule,
+    RakutenModule,
+  ],
   providers: [BooksService, BooksResolver],
   exports: [BooksService],
 })
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,12 +1,16 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {ConfigType} from '@nestjs/config';
 import {ExcludeService} from '../exclude/exclude.service';
 import {OpenBDService} from '../openbd/openbd.service';
 import {RakutenService} from '../rakuten/rakuten.service';
 import {RedisCacheService} from '../redis-cache/redis-cache.service';
+import {BooksConfig} from './books.config';
 
 @Injectable()
 export class BooksService {
   constructor(
+    @Inject(BooksConfig.KEY)
+    private config: ConfigType<typeof BooksConfig>,
     private cache: RedisCacheService,
 
     private readonly excludeService: ExcludeService,
@@ -16,23 +20,21 @@ export class BooksService {
   ) {}
 
   async getFromISBN(isbn: string): Promise<string | null> {
-    const key = isbn.replace(/-/g, '');
+    const dehyphenized = isbn.replace(/-/g, '');
+    const cachedKey = `isbn:${dehyphenized}`;
 
-    const cached = await this.cache.get<string>(key);
-    if (cached) {
-      await this.cache.set(key, cached);
-      return cached;
-    }
+    const cached = await this.cache.get<string>(cachedKey);
+    if (cached) return cached;
 
-    const openBD = await this.oepnBDService.getBookCover(key);
+    const openBD = await this.oepnBDService.getBookCover(dehyphenized);
     if (openBD && !(await this.excludeService.canExcludeFromUrl(openBD))) {
-      await this.cache.set(key, openBD);
+      await this.cache.set(cachedKey, openBD);
       return openBD;
     }
 
-    const rakuten = await this.rakutenService.getBookCover(key);
+    const rakuten = await this.rakutenService.getBookCover(dehyphenized);
     if (rakuten && !(await this.excludeService.canExcludeFromUrl(rakuten))) {
-      await this.cache.set(key, rakuten);
+      await this.cache.set(cachedKey, rakuten);
       return rakuten;
     }
 
@@ -41,5 +43,9 @@ export class BooksService {
 
   async getCover({isbn}: {isbn?: string}): Promise<string | null> {
     return (isbn && (await this.getFromISBN(isbn))) || null;
+  }
+
+  urlProxy(url: string): string {
+    return new URL(`/${url}`, this.config.imageproxyBaseUrl).toString();
   }
 }
