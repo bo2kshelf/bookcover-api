aea9cab07941b5be43a47b9e9cf9098577e2cc55
SnO2WMaN <me@sno2wman.net>
2021-03-16 09:07:51 +0900

    refactor: 環境変数の名前など変更

--- src/redis-cache/redis-cache.config.ts
+++ src/redis-cache/redis-cache.config.ts
@@ -1,7 +1,7 @@
 import {registerAs} from '@nestjs/config';
 
 export const RedisCacheConfig = registerAs('redis-cache', () => ({
-  host: process.env.REDIS_CACHE_HOST!,
-  port: Number.parseInt(process.env.REDIS_CACHE_PORT!, 10),
+  host: process.env.REDIS_HOST!,
+  port: Number.parseInt(process.env.REDIS_PORT!, 10),
   ttl: 60 * 60 * 24 * 7,
 }));
